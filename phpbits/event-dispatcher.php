65d61532a60fb2e6d445963905461114c07f04f7
navinweb <anatoliykirzo@gmail.com>
2018-09-22 22:39:31 +0300

    Visual Debt

---
+++ phpbits/event-dispatcher.php
@@ -0,0 +1,57 @@
+<?php
+
+interface EventDispatcher
+{
+    /**
+     * listen
+     *
+     * @param  mixed $name
+     * @param  mixed $handler
+     *
+     */
+    public function listen(string $name, callable $handler);
+
+    /**
+     * fire
+     *
+     * @param  mixed $name
+     *
+     * @return bool
+     */
+    public function fire(string $name): bool;
+}
+
+class SyncDispatcher implements EventDispatcher
+{
+    protected $events = [];
+
+    public function listen(string $name, callable $handler)
+    {
+        $this->events[$name][] = $handler;
+    }
+
+    public function fire(string $name): bool
+    {
+        if (!array_key_exists($name, $this->events)) {
+            return false;
+        }
+
+        foreach ($this->events[$name] as $event) {
+            $event();
+        }
+
+        return true;
+    }
+}
+
+$event = new SyncDispatcher;
+
+$event->listen('subscribed', function () {
+    echo 'handling it';
+});
+
+$event->listen('subscribed', function () {
+    echo 'handling it again';
+});
+
+$event->fire('subscribed');
